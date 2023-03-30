b8a8e7e6d2fa97246ab2985548b4a2b6e04c94ef
degero <chaddotnet@outlook.com>
2023-03-30 13:32:05 +0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<div>Hello 1 {{message}}</div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'swatest';
+
+  message = '';
+
+  constructor(private http: HttpClient) {
+    this.http.get('/api/message')
+      .subscribe((resp: any) => {
+        this.message = resp;
+      }, (error: any) => {
+        console.log("ERROR");
+      });
+  }
 }
